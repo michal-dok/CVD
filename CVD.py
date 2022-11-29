75c49f33e313e711f9c96024f180fd4911501e27
michal-dok <michaldok9@gmail.com>
2022-11-29 10:23:22 +0100

    review

--- CVD.py
+++ CVD.py
@@ -6,14 +6,19 @@ from sklearn.utils.extmath import randomized_svd as rSVD
 
 class CVD:
 
-    def __init__(self, ):
+    def __init__(self, rY=1, rCr=1, rCb=1):
+        self.rY = rY
+        self.rCr = rCr
+        self.rCb = rCb
         self.video_matrix = None
+        self.approx_matrix = None
+        self.decoded_video = None
         self.frame_rows = None
         self.frame_cols = None
         self.fps = None
-        self.compressed = None
 
-    def encode(self, path, rate=(1, 1, 1)):
+
+    def encode(self, path):
         cap = cv2.VideoCapture(path)
         (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
         if int(major_ver) < 3:
@@ -28,15 +33,15 @@ class CVD:
         while cap.isOpened() and ret:
             ret, frame = cap.read()
             if ret:
-                ycbcr_im = cv2.cvtColor(frame, cv2.COLOR_BGR2YCR_CB)
-                y, cb, cr = cv2.split(ycbcr_im)
-                y //= rate[0]
-                cb //= rate[1]
-                cr //= rate[2]
+                YCrCb_im = cv2.cvtColor(frame, cv2.COLOR_BGR2YCR_CB)
+                y, cr, cb = cv2.split(YCrCb_im)
+                y //= self.rY
+                cr //= self.rCr
+                cb //= self.rCb
                 if self.frame_rows is None and self.frame_cols is None:
                     self.frame_rows, self.frame_cols = y.shape
                     print(self.frame_rows, self.frame_cols)
-                col = np.concatenate((y.ravel(), cb.ravel(), cr.ravel()))
+                col = np.concatenate((y.ravel(), cr.ravel(), cb.ravel()))
                 vid.append(col)
 
         video_matrix = np.array(vid, order='c').T
@@ -44,8 +49,8 @@ class CVD:
         print(self.video_matrix.shape)
 
 
-    def compress(self, rank=50):
-        print("compressing ...")
+    def approx(self, rank=50):
+        print("approx ...")
         U, S, VT = LA.svd(self.video_matrix, False)
         print("SVD done!")
 
@@ -56,38 +61,35 @@ class CVD:
         #for i in range(rank):
         #    Ar += S[i] * np.outer(U[i], VT[i])
 
-        self.video_matrix = Ar
-        print("compression done!")
+        self.approx_matrix = Ar
+        print("approx done!")
 
 
-    def decode(self, rate=(1, 1, 1)):
+    def decode(self, matrix):
         print("decoding ...")
         decoded = []
-        for col in self.video_matrix.T:
+        for col in matrix.T:
             y, cr, cb = np.array_split(col, 3)
-            y = np.around(y*rate[0])
-            cr = np.around(cr*rate[1])
-            cb = np.around(cb*rate[2])
-            ycbcr_im = np.dstack([vec.reshape(self.frame_rows, self.frame_cols) for vec in (y, cr, cb)])
-            ycbcr_im[ycbcr_im <= 0] = 0
-            ycbcr_im[ycbcr_im >= 255] = 255
-            ycbcr_im = (np.rint(ycbcr_im)).astype(np.uint8)
-
-            print("shape:", ycbcr_im.shape)
-            print(np.max(ycbcr_im), np.min(ycbcr_im))
-            #cv2.imshow('FrameYCC', ycbcr_im)
+            y = np.around(y*self.rY)
+            cr = np.around(cr*self.rCr)
+            cb = np.around(cb*self.rCb)
+            YCrCb_im = np.dstack([vec.reshape(self.frame_rows, self.frame_cols) for vec in (y, cr, cb)])
+            YCrCb_im[YCrCb_im <= 0] = 0
+            YCrCb_im[YCrCb_im >= 255] = 255
+            YCrCb_im = (np.rint(YCrCb_im)).astype(np.uint8)
+            #cv2.imshow('FrameYCC', YCrCb_im)
             #cv2.waitKey(0)
-            bgr_im = cv2.cvtColor(ycbcr_im, cv2.COLOR_YCrCb2BGR)
+            bgr_im = cv2.cvtColor(YCrCb_im, cv2.COLOR_YCrCb2BGR)
             decoded.append(bgr_im)
             #cv2.imshow('Frame', bgr_im)
             #cv2.waitKey(0)
-        self.compressed = decoded
+        self.decoded_video = decoded
         print("decoded")
 
     def write(self, filename):
         print(f"saving to {filename}")
         out = cv2.VideoWriter(filename, cv2.VideoWriter_fourcc(*'mp4v'), round(self.fps), (self.frame_cols, self.frame_rows))
-        frames = self.compressed
+        frames = self.decoded_video
         for frame in frames:
             out.write(frame)
         out.release()
@@ -98,10 +100,10 @@ if __name__ == "__main__":
     rate = (2, 4, 4)
     rank = 50
     output_name = f"compressd_rate-{''.join(str(num) for num in rate)}_rank-{rank}.mp4"
-    cvd = CVD()
-    cvd.encode("coko.webm", rate=rate)
-    cvd.compress(rank=rank)
-    cvd.decode(rate=rate)
+    cvd = CVD(*rate)
+    cvd.encode("coko.webm")
+    cvd.approx(rank=rank)
+    cvd.decode(cvd.approx_matrix)
     cvd.write(output_name)
 
 
---
+++ notes.md
@@ -0,0 +1,6 @@
+Postupne rSVD
+    - co bude vysledok
+    - ako efektivne volit krok
+
+Format komprimovanych dat
+    - opatovne rozklad?
